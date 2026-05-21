f55682ab184f8367b523e5126dc73ce5154a1c19
iamcris28 <goldmaster288pro@gmail.com>
2026-05-20 19:13:31 -0600

    Forzar resolucion IPv4 en Nodemailer para evitar error ENETUNREACH

--- config/email.js
+++ config/email.js
@@ -1,11 +1,15 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-// Configuración robusta y explícita para servidores en la nube (Render)
+// 🌟 SOLUCIÓN TÉCNICA: Obligamos a Node.js a priorizar IPv4 sobre IPv6
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
+// Tu configuración segura
 const transcoder = nodemailer.createTransport({
-  host: 'smtp.gmail.com', // Dirección exacta de Google
-  port: 465,              // Puerto seguro SSL
-  secure: true,           // Usar encriptación
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS  
